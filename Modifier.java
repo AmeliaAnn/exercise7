6ba7cc9a37101830d8764b89e37fe332e3bdab19
AmeliaAnn <53219102+AmeliaAnn@users.noreply.github.com>
2019-10-18 20:38:12 +0300

    Create Modifier.java

---
+++ Modifier.java
@@ -0,0 +1,10 @@
+package exercise7;
+
+public final class Modifier {
+
+
+      public static final int ONE = 1;
+      public static final int TWO = 2;
+      public static final int THREE = 3;
+      
+      }
